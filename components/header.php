165d18cd94047a1f197e52b8ff83778982a2f4a9
iballi2017 <iballi2017@gmail.com>
2021-02-24 16:35:01 +0100

    first commit

---
+++ components/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Worktool Survey</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Custom css -->
+    <link rel="stylesheet" href="../stylesheet/css/style.min.css">
+</head>
+
+<body>
+    <!-- <header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Carousel</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</header> -->
+
+    <!-- header 2 -->
+    <header class="">
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-2 custom-navbar shadow-sm">
+            <h5 class="my-0 mr-md-auto font-weight-normal brand-component">
+                <img src="../assets/images/settings-group-icon.png" class="img-fluid brand-logo-sm" alt="">
+                <a href="../" class="brand-tagline">Work Tool Survey</a>
+            </h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+                <div>
+                    <div class="dropdown default-bg">
+                        <div class="btn dropdown-toggle border rounded-0 w-100 uppercase" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="mr-5">Select a form</span>
+                        </div>
+                        <div class="dropdown-menu default-bg" aria-labelledby="dropdownMenuButton" style="margin-left:-100% !important">                            
+                        <a class="dropdown-item uppercase" href="../views/technology-equipment-repair-request.php">Technology equipment repair request</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item uppercase" href="../views/non-technology-equipment-repair-request.php">Non-technology equipment repair request</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item uppercase" href="../views/vehicles-and-logistics-repair-request.php">Vehicles and logistics repair request</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item uppercase" href="../views/other-repair-request.php">Other repair requests</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
---
+++ views/vehicles-and-logistics-repair-request.php
@@ -0,0 +1,45 @@
+<!-- Header -->
+<?php include '../components/header.php'; ?>
+
+<main id="vehiocles-and-logistics-repair-request">
+    <div class="title-wrapper py-2">
+        <h1 class="page-title uppercase display-6 text-center text-white my-0">Vehicles and logistics repair request</h1>
+    </div>
+    <div class="container">
+
+        <div class="form-wrapper my-5">
+            <form>
+
+                <div class="row form-group">
+                    <div class="col-12 col-md-6 mb-3 mb-md-0">
+                        <input type="text" class="form-control" placeholder="Date of request">
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <input type="text" class="form-control" placeholder="Requested by">
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-12 col-md-6 mb-3 mb-md-0">
+                        <input type="text" class="form-control" placeholder="Primary User">
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <input type="text" class="form-control" placeholder="Asset name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="location" aria-describedby="location" placeholder="Location">
+                    <small id="emailHelp" class="form-text text-danger">This field is required.</small>
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="6" placeholder="Description of fault or damage"></textarea>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-md btn-primary px-5">Send Request</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+
+<!-- Footer -->
+<?php include '../components/footer.php'; ?>
